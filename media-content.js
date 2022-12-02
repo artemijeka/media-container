980da625edb6c3a7c5616cc72786312253fd8c04
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2022-12-02 22:49:57 +0400

    init

---
+++ media-content.js
@@ -0,0 +1,41 @@
+/**
+ * data-media-container - обязательно установить контейнеру
+ */
+function MediaContainer(params) {
+  let dataContainerAll = document.querySelectorAll("[data-media-container]");
+  setMedias();
+
+  var setMediasTimeout;
+  window.addEventListener("resize", function () {
+    clearTimeout(setMediasTimeout);
+    setMediasTimeout = setTimeout(function () {
+      setMedias();
+    }, params.resizeTimeout);
+  });
+
+  function setMedias() {
+    dataContainerAll.forEach(function (container) {
+      let mediasMore = [];
+      let mediasLess = [];
+      let mediasAll = "";
+
+      let containerRect = container.getBoundingClientRect();
+      console.log("containerRect.width");
+      console.log(containerRect.width);
+      params.medias.forEach(function (media) {
+        if (containerRect.width < media) {
+          mediasLess.push("<" + Number(media - 1));
+        }
+        if (containerRect.width >= media) {
+          mediasMore.push(">=" + media);
+        }
+      });
+
+      mediasAll += mediasLess.join(",");
+      mediasAll += ",";
+      mediasAll += mediasMore.join(",");
+
+      container.dataset.mediaContainer = mediasAll;
+    });
+  }
+}
